fca41f3fcb440d2018ac637d2e606b1ae671376b
rpassaropezua <passaro.raymundo@gmail.com>
2023-03-29 19:10:52 -0500

    change for the cli.js to work with lambda (the dockerfile checks out this specific branch)

--- AWS/AWSLambda1/Function.cs
+++ AWS/AWSLambda1/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using System.Diagnostics;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -14,10 +15,36 @@ public class Function
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public Casing FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(string input, ILambdaContext context)
     {
-        return new Casing(input.ToLower(), input.ToUpper());
+        if (input == null || input == "")
+        {
+            input = "-help";
+        }
+
+        //var npmGlobalLocation = GetNpmGlobalLocation();
+
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "node",
+            Arguments = $"./aicapture/cli.js {input}", // Update the path according to the package
+            RedirectStandardOutput = true,
+            RedirectStandardInput = true,
+            //RedirectStandardError = true,
+            //UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using var process = new Process { StartInfo = startInfo };
+        process.Start();
+        string result = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        if (!process.HasExited && process.StandardInput.BaseStream.CanWrite)
+        {
+            process.StandardInput.WriteLine("a");
+            process.StandardInput.Flush();
+        }
+        return result;
     }
 }
-
-public record Casing(string Lower, string Upper);
--- cli.js
+++ cli.js
@@ -3,7 +3,5 @@
 'use strict';
 
 const { spawn } = require('child_process');
-var path = require('path');
-var appDir = path.dirname(require.main.filename);
 
-spawn('dotnet', [`${appDir}/Windows/CLI/bin/Release/netcoreapp3.1/AICapture.OST.CLI.dll`, process.argv.slice(2).join(' ')], { stdio: 'inherit' });
+spawn('dotnet', [`/var/task/aicapture/Windows/CLI/bin/Release/netcoreapp3.1/AICapture.OST.CLI.dll`, process.argv.slice(2).join(' ')], { stdio: 'inherit' });
