1ec3d27903f8efb51d937e5d6ed4b93b087b6933
rpassaropezua <passaro.raymundo@gmail.com>
2023-03-31 20:40:13 -0500

    starting to add changes to deal with xml file set inputs

--- AWS/AWSLambda2/index.js
+++ AWS/AWSLambda2/index.js
@@ -21,6 +21,15 @@ exports.handler = async (event) => {
     fs.writeFileSync(filePath, '{}');
     process.env.CurrentWorkingDirectory = dirPath;
     const input = event.body || '-help';
+    
+    //check if input is xml, if it is write it to tmp folder
+    if (event.body.startsWith('<')) {
+        const xmlFilePath = `${dirPath}/transpilerequest.xml`;
+        fs.writeFileSync(xmlFilePath, event.body);
+        //read the file, if there is a fileset node, write those out to their own files
+
+    }
+
     return new Promise((resolve, reject) => {
         const aicProcess = spawn('aic', [input, `-p path=${dirPath}`, `-sc`]);
         log("current input:", input);
@@ -39,8 +48,9 @@ exports.handler = async (event) => {
             //find a .xml file, and write it to the stdout
             const xmlFile = fs.readdirSync(dirPath).find(file => file.endsWith('.xml'));
             if (xmlFile) {
+                const xmlContents = fs.readFileSync(`${dirPath}/${xmlFile}`, 'utf8');
                 //write the xmlfile into the output variable
-                output = xmlFile;
+                output = xmlContents;
             }
             if (code === 0) {
                 resolve({
