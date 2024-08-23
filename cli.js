3049110ebdbec3d54f5a7ae1f3e5cb605461e1a3
eejai42 <ej@ssot.me>
2024-08-22 19:01:30 -0500

    MOVED CLI TO github.com:ssotme/cli

--- cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-const { spawn } = require('child_process');
-var path = require('path');
-var appDir = path.dirname(require.main.filename);
-
-spawn('dotnet', [`${appDir}/Windows/CLI/bin/Release/net7.0/AICapture.OST.CLI.dll`, process.argv.slice(2).join(' ')], { stdio: 'inherit' });
